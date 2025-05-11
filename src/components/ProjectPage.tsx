71ed6fb53d77e8cd6aeb30219b0a8554e74f64e8
Max Lair <max@lairfamily.com>
2025-05-11 01:03:59 -0400

    SOLUTON

--- src/components/ProjectPage.tsx
+++ src/components/ProjectPage.tsx
@@ -1,14 +1,46 @@
 // pages/project.tsx
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
 import ExampleMDX from '../docs/example.mdx';
+import exampleMdxContent from '../docs/example.mdx?raw';
 import matter from 'gray-matter';
+import { Buffer } from 'buffer';
 
-const ProjectPage = ({ frontmatter, content }: any) => {
+// Import the raw MDX content as a string
+
+window.Buffer = Buffer;
+
+const ProjectPage = () => {
+
+  function JSONtoMarkdown({ jsonData }) {
+    const formattedJson = JSON.stringify(jsonData, null, 2);
+
+    return (
+      <pre>
+        {formattedJson}
+      </pre>
+    );
+  }
+
+  const { data: frontmatter, content } = matter(exampleMdxContent);
+  
+  console.log('Frontmatter:', frontmatter);
+  console.log('Content:', content);
+
+  //NOTE: Add array to the frontmatter for gallery, and inline iamges will be formated regularly.
   return (
-    <div className="flex flex-col lg:flex-row h-full w-full p-4 space-y-8 lg:space-y-0 lg:space-x-8">
-      {/* MDX Content */}
-      <div className="prose prose-neutral dark:prose-invert max-w-none w-full lg:w-1/2 overflow-auto">
-        <ExampleMDX />
+    <div className="flex flex-col h-[100vh] relative w-full">
+      {/* cover */}
+      <div>
+        <img className="relative z-[-2] h-[100vh] w-full object-cover" src={`${frontmatter.coverImage}`}/>
+        <h1 className="text-[75px] text-highlight p-4 font-mono absolute bottom-0 z-[5]">{frontmatter.title}:<br/> <span className="font-serif">{frontmatter.description}</span></h1>
+      </div>
+      <div className="mt-[250px] px-5 bg-red">
+        <div className="prose prose-neutral dark:prose-invert max-w-none w-full overflow-auto">
+          <ReactMarkdown>
+            {content}
+          </ReactMarkdown>
+        </div>
       </div>
     </div>
   );
